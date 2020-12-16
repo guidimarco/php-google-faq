b1935be737bee6e2fc82999b2f6eadef03f7b579
Marco Guidi <mark.88g@gmail.com>
2020-12-16 13:22:31 +0100

    init proj

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>php-google-faq</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
